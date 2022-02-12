2223954a597f52ae882a669024e104f41cd97b01
David-Kenkadze <91611526+David-Kenkadze@users.noreply.github.com>
2022-02-12 19:55:51 +0400

    Update solution.go

--- solution.go
+++ solution.go
@@ -3,14 +3,14 @@ package solution
 import (
 	"github.com/kyokomi/emoji/v2"
 
-	"fmt"
+	
 )
 
 
 func GetMessage() string {
-        hello_message := emoji.Sprint("hello :world_map:!")
-        fmt.Println(hello_message)
-        return "hello-message"
+          
+        
+        return emoji.Sprint("hello :world_map:!")
 }
 
 
