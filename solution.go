df57694e92b4b772f728e34de117aba9f9013559
David-Kenkadze <91611526+David-Kenkadze@users.noreply.github.com>
2022-02-12 19:12:20 +0400

    Update solution.go

--- solution.go
+++ solution.go
@@ -7,7 +7,7 @@ import (
 )
 
 func GetMessage() string {
-	GetMessage := emoji.Sprint("hello world :world_map:!!")
+	GetMessage := emoji.Sprint("hello :world_map:!!")
 	fmt.Println(GetMessage)
 	return GetMessage
 }
