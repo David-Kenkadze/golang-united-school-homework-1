b6644fb0202cd6a3aa0ed143bca3e7391e2ec225
David-Kenkadze <dkenk2019@agruni.edu.ge>
2022-02-12 18:37:57 +0400

    Davit Kenkadze first untested solution

--- solution.go
+++ solution.go
@@ -1,5 +1,13 @@
 package solution
 
+import (
+	"github.com/kyokomi/emoji/v2"
+
+	"fmt"
+)
+
 func GetMessage() string {
-	return ""
+	hello_message := emoji.Sprint("hello world :world_map:!!")
+	fmt.Println(hello_message)
+	return "0"
 }
